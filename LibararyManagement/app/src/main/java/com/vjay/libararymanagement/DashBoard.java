382d235299c650e428014a4a16df7a0dbe3877b5
Jayaramananan <librarymanagement702>
2020-03-16 21:49:30 +0530

    Created dashboard page

---
+++ LibararyManagement/app/src/main/java/com/vjay/libararymanagement/DashBoard.java
@@ -0,0 +1,13 @@
+package com.vjay.libararymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
+}
